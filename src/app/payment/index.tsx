dbec3fbe8da59146438e28fe99f25cb5a5798d3a
souzannatha <souzanatha04@gmail.com>
2024-04-16 19:52:44 -0300

    configuração inicial

---
+++ src/app/payment/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Payment() {
+  return (
+    <View style={styles.container}>
+      <Text>Oi</Text>
+    </View>
+  );
+}
