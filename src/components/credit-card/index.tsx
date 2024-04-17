8b9903177db4fa89330317f72eb523518325aabe
souzannatha <souzanatha04@gmail.com>
2024-04-17 10:31:30 -0300

    verso do cartão feito

--- src/components/credit-card/index.tsx
+++ src/components/credit-card/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Text, View, Image, ImageBackground } from "react-native";
 import { styles } from "./style";
-import { LinearGradient } from "expo-linear-gradient";
 
 export function CreditCard() {
   return (
-    <ImageBackground
-      style={styles.card}
-      source={require("../../assets/images/bg1.png")}
-    >
-      <View style={styles.header}>
+    <View>
+      <ImageBackground
+        style={styles.cardContainer}
+        source={require("../../assets/images/bg1.png")}
+      >
         <View style={styles.flag}>
           <Image
             style={styles.circles}
@@ -18,7 +17,29 @@ export function CreditCard() {
           <Text style={styles.mycard}>Meu cartão</Text>
         </View>
         <Text style={styles.name}>Nathã Souza Lopes</Text>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+      <ImageBackground
+        style={[styles.cardContainer]}
+        source={require("../../assets/images/bg2.png")}
+      >
+        <View style={styles.flag}>
+          <Image
+            style={styles.circles}
+            source={require("../../assets/images/circles.png")}
+          />
+        </View>
+        <View style={styles.cardDetails}>
+          <Text style={styles.textDetails}>CVV</Text>
+          <Text style={styles.textDetails}>123</Text>
+          <View style={styles.footer}>
+            <View>
+              <Text style={styles.textDetails}>Número do cartão</Text>
+              <Text>270603981020356 1996</Text>
+            </View>
+            <Text style={styles.validate}>04/24</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
--- src/components/credit-card/style.ts
+++ src/components/credit-card/style.ts
@@ -1,29 +1,49 @@
 import { StyleSheet } from "react-native";
-import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 export const styles = StyleSheet.create({
-  header: {},
-  card: {
+  cardContainer: {
     width: 354,
     height: 230,
     borderRadius: 15,
     padding: 16,
     margin: 20,
   },
-  front: {},
-  back: {},
+  flag: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  circles: {
+    width: 54,
+    height: 35,
+  },
   mycard: {
     alignSelf: "center",
     fontSize: 14,
     fontWeight: "500",
     marginLeft: 8,
   },
-  circles: { width: 54, height: 35 },
-  flag: { flexDirection: "row", gap: 5 },
   name: {
     alignSelf: "center",
     marginTop: 35,
     fontSize: 32,
     fontWeight: "bold",
+  },
+  cardDetails: {
+    gap: 8,
+    marginTop: 10,
+  },
+  textDetails: {
+    fontSize: 20,
+    fontWeight: "500",
+    marginBottom: 8,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    alignContent: "center",
+  },
+  validate: {
+    marginTop: 35,
   },
 });
